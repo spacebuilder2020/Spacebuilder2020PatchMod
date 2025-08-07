f24549b1d18c94498ce42ea76f1c69b8131e5469
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2025-08-06 18:31:16 -0600

    Initial Commit

---
+++ NetworkingFixMod.cs
@@ -0,0 +1,77 @@
+﻿using Assets.Scripts;
+using HarmonyLib;
+using StationeersMods.Interface;
+using System.IO;
+using System;
+using Assets.Scripts.Localization2;
+using Assets.Scripts.Networking;
+using Assets.Scripts.Objects;
+using Assets.Scripts.Util;
+using UnityEngine;
+using Util.Commands;
+
+namespace NetworkingFixMod
+{
+    
+    [StationeersMod("NetworkingFixMod", "NetworkingFixMod [StationeersMods]", "1.0.0")]
+    class NetworkingFixMod : ModBehaviour
+    {
+        public override void OnLoaded(ContentHandler contentHandler)
+        {
+            //READ THE README FIRST! 
+            
+            //Config example
+            // configBool = Config.Bind("Input",
+            //     "Boolean",
+            //     true,
+            //     "Boolean description");
+            
+            Harmony harmony = new Harmony("NetworkingFix");
+            harmony.PatchAll();
+            ConsoleWindow.Print("NetworkingFix Loaded!");
+        }
+    }
+
+    [HarmonyPatch]
+    class NetworkServerPatch
+    {
+        [HarmonyPatch(typeof(NetworkServer), "HandleBlacklisting"), HarmonyPostfix]
+        static void NetworkServer_HandleBlacklisting(ref NetworkMessages.VerifyPlayer msg, ref Client client) =>
+            NetworkManager.CloseP2PConnectionServer(client);
+
+        [HarmonyPatch(typeof(NetworkServer), "HandleIncorrectPassword"), HarmonyPostfix]
+        static void NetworkServer_HandleIncorrectPassword(ref Client client) =>
+            NetworkManager.CloseP2PConnectionServer(client);
+
+        [HarmonyPatch(typeof(NetworkServer), "HandleIncorrectVersion"), HarmonyPostfix]
+        static void NetworkServer_HandleIncorrectVersion(ref Client client, NetworkMessages.VerifyPlayer msg) => NetworkManager.CloseP2PConnectionServer(client);
+
+        [HarmonyPatch(typeof(KickCommand), "Kick"), HarmonyPrefix]
+        static bool KickCommand_Kick(ref string[] lineSplit)
+        {
+            if (!NetworkManager.IsActiveAsClient && (NetworkManager.IsClient || NetworkManager.IsServer) && lineSplit.Length == 1 && !int.TryParse(lineSplit[0], out int ignored))
+            {
+                string name = lineSplit[0];
+                string clientId = NetworkBase.Clients.Find(client => client.name == name)?.ClientId.ToString();
+                if (clientId != null)
+                {
+                    lineSplit[0] = clientId;
+                }
+                else
+                {
+                    ConsoleWindow.PrintError($"Unable to find client by name: {name}", true);
+                    
+                    var clients = NetworkBase.Clients.FindAll(client => client.name.ToLower().StartsWith(name.ToLower()) || client.name.ToLower().Contains(name.ToLower()));
+                    if (clients.Count > 0)
+                    {
+                        ConsoleWindow.Print("Possible Options:");
+                        clients.ForEach(client => ConsoleWindow.Print(client.name));
+                    }
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# This is a StationeersLaunchPad Plugin Mod. It requires Bepinex to be installed with the StationeersLaunchPad plugin.
+See: https://github.com/StationeersLaunchPad/StationeersLaunchPad
+
+    Multiple Network Related Patches including:
+
+    Patches connections to prevent a timeout when a client is kicked at initial connection. (Invalid password, blacklisting, invalid version).
+    
+    Extends Kick command so that a player name can be specified instead of a steamid.
+      Requires exact name, if name is not exact, will try to find possible options and print them out.
