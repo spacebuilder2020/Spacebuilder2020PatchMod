8765afb4c3d27abec6dce09e5f1ff283c9c80203
Spacebuilder2020 <spacebuilder2020@users.noreply.github.com>
2025-10-21 17:18:19 -0600

    Implement Alpha logic for loading different patches based on different game versions

--- Spacebuilder2020PatchMod.cs
+++ Spacebuilder2020PatchMod.cs
@@ -1,14 +1,12 @@
 ﻿using Assets.Scripts;
 using HarmonyLib;
 using StationeersMods.Interface;
-using System.IO;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Assets.Scripts.Localization2;
 using Assets.Scripts.Networking;
 using Assets.Scripts.Objects;
-using Assets.Scripts.Util;
 using Steamworks;
 using UnityEngine;
 using Util.Commands;
@@ -23,13 +21,44 @@ namespace Spacebuilder2020PatchMod
         {
             ConsoleWindow.Print("Loading Patches for Spacebuilder2020's PatchMod");
             Harmony harmony = new Harmony("Spacebuilder2020PatchMod");
-            harmony.PatchAll();
+            var gameAssembly = typeof(GameManager).Assembly;
+            
+            Debug.Log($"Game Version: {gameAssembly.GetName().Version}");
+            var assembly = typeof(Spacebuilder2020PatchMod).Assembly;
+            AccessTools.GetTypesFromAssembly(assembly).Do(type =>
+            {
+
+                var ver = type.GetCustomAttributes(true).OfType<GameVersion>().FirstOrDefault();
+                var version = gameAssembly.GetName().Version;
+                
+                if (ver != null && (ver.MinVersion > version || ver.MaxVersion < version) )
+                {
+                    Debug.Log($"Patch for {type.Name} ignored because game version does not match!");
+                    Debug.Log($"Type: {type.Name} Build: {version} Min: {ver?.MinVersion} Max: {ver?.MaxVersion}");
+                    return;
+                }
+                var processor = harmony.CreateClassProcessor(type);
+                
+                processor.Patch();
+            });
             ConsoleWindow.Print("Patches Loaded!");
         }
     }
+    [AttributeUsage(AttributeTargets.Class)]
+    class GameVersion : Attribute
+    {
+        public Version MinVersion;
+        public Version MaxVersion;
 
-    [HarmonyPatch]
-    class Spacebuilder2020Patches
+        public GameVersion(string minVersion, string maxVersion)
+        {
+            MinVersion = new Version(minVersion);
+            MaxVersion = new Version(maxVersion);
+        }
+    }
+    
+    [HarmonyPatch, GameVersion("0.0.0.0","0.2.5958.26183")]
+    class Spacebuilder2020PatchesLegacy
     {
         [HarmonyPatch(typeof(NetworkServer), "HandleBlacklisting"), HarmonyPostfix]
         static void NetworkServer_HandleBlacklisting(ref NetworkMessages.VerifyPlayer msg, ref Client client) => DelayedClose(client);
@@ -104,6 +133,77 @@ namespace Spacebuilder2020PatchMod
                     (int) ((__instance.DamageState.MaxDamage - (double) __instance.DamageState.Decay) / ( __instance.CurrentDecayRate * 60.0)))
                 .GetValue();
             return false;
+        }
+    }
+    
+    [HarmonyPatch, GameVersion("0.2.5958.26183","0.2.9999.99999")]
+    class Spacebuilder2020PatchesPost26183
+    {
+        [HarmonyPatch(typeof(NetworkServer), "HandleBlacklisting"), HarmonyPostfix]
+        static void NetworkServer_HandleBlacklisting(ref NetworkMessages.VerifyPlayer msg, ref Client client) => DelayedClose(client);
+
+        [HarmonyPatch(typeof(NetworkServer), "HandleIncorrectPassword"), HarmonyPostfix]
+        static void NetworkServer_HandleIncorrectPassword(ref Client client) => DelayedClose(client);
+
+        [HarmonyPatch(typeof(NetworkServer), "HandleIncorrectVersion"), HarmonyPostfix]
+        static void NetworkServer_HandleIncorrectVersion(ref Client client, NetworkMessages.VerifyPlayer msg) => DelayedClose(client);
+
+        static void DelayedClose(Client client)
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(500);
+                NetworkManager.CloseP2PConnectionServer(client);
+            });
+        }
+        [HarmonyPatch(typeof(KickCommand), "Kick"), HarmonyPrefix]
+        static bool KickCommand_Kick(ref string[] lineSplit)
+        {
+            if (!NetworkManager.IsActiveAsClient && (NetworkManager.IsClient || NetworkManager.IsServer) && lineSplit.Length == 1)
+            {
+                string nameOrID = lineSplit[0];
+                bool parsed = ulong.TryParse(lineSplit[0], out var clientId); 
+                Client user = parsed ? Client.Find(clientId) : NetworkBase.Clients.Find(client => client.name == nameOrID);
+
+                if (user != null)
+                {
+                    ConsoleWindow.PrintAction($"client '{user.name}' kicked from game");
+                    user.Disconnect();
+                }
+                else
+                {
+                    ConsoleWindow.PrintError($"Unable to find client by {(parsed ? "id" :"name")}: '{nameOrID}' in list", true);
+                    
+                    if (!parsed)
+                    {
+                        var clients = NetworkBase.Clients.FindAll(client =>
+                            client.name.ToLower().StartsWith(nameOrID.ToLower()) ||
+                            client.name.ToLower().Contains(nameOrID.ToLower()));
+                        if (clients.Count > 0)
+                        {
+                            ConsoleWindow.Print("Possible Options:");
+                            clients.ForEach(client => ConsoleWindow.Print(client.name));
+                        }
+                    }
+                    else
+                    {
+                        ConsoleWindow.PrintAction("Trying a kick by exact steam id");
+                        
+                        if (SteamNetworking.CloseP2PSessionWithUser(clientId))
+                        {
+                            ConsoleWindow.Print("Successfully kicked by Steam!");
+                        }
+                        else
+                        {
+                            ConsoleWindow.PrintError("Error kicking by id, user may not be connected!", true);
+                        }
+                        
+                    }
+                }
+                return false;
+            }
+
+            return true;
         }
     }
 }
